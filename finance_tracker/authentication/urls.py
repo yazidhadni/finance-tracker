f1e4ee6877ffc6d8b5ee03ac555a0178c21c4f90
yazidhadni <yazid.hadni98@gmail.com>
2024-01-02 10:36:10 +0100

    add password change view and success page

--- finance_tracker/authentication/urls.py
+++ finance_tracker/authentication/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import (
+    LoginView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+)
 
 from . import views
 
@@ -12,6 +16,20 @@ urlpatterns = [
         name="login",
     ),
     path("logout/", views.logout_user, name="logout"),
+    path(
+        "change-password/",
+        PasswordChangeView.as_view(
+            template_name="authentication/password_change_form.html"
+        ),
+        name="password_change",
+    ),
+    path(
+        "change-password-done/",
+        PasswordChangeDoneView.as_view(
+            template_name="authentication/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
     path("home/", views.home, name="home"),
     path("sign-up/", views.register, name="register"),
 ]
